f88194d9d66a0a698627c5f75612ac0991e0fb0c
Chanple <chanple@outlook.com>
2021-06-26 08:34:28 +0800

    ReleaseMutex

--- MemoryBall/App.xaml.cs
+++ MemoryBall/App.xaml.cs
@@ -24,14 +24,5 @@ namespace MemoryBall
 
             base.OnStartup(e);
         }
-
-        /// <summary>Raises the <see cref="E:System.Windows.Application.Exit" /> event.</summary>
-        /// <param name="e">An <see cref="T:System.Windows.ExitEventArgs" /> that contains the event data.</param>
-        protected override void OnExit(ExitEventArgs e)
-        {
-            _mutex.ReleaseMutex();
-
-            base.OnExit(e);
-        }
     }
 }
